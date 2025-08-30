1d7268a539e741a9418cdfcc022464f713ec9fa9
Snehal Pal <snehalpal2007@gmail.com>
2025-08-30 11:25:56 +0530

    Add files via upload

---
+++ program_5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float ( P , R , T);
+    printf("enter principal,rate,time");
+    scanf("%f , %f , %f , &r , &t ,&p");
+
+    simple interest =  (P*R*T/100);
+    printf("simple interest .2f\n", simple interest );
+
+return 0;
+
+
+}
