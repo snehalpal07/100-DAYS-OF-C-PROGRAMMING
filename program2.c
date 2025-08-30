5dcd56210acf3b332499dcffb20c6cad2cbd73ff
Snehal Pal <snehalpal2007@gmail.com>
2025-08-30 11:27:50 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter 2 number: ");
+    scanf("%d , %d , &a , &b ");
+
+    printf("the sum of a and b\n", a+b);
+    printf("the differnce of a and b\n", a-b);
+    printf("the product of a*b\n", a*b);
+    printf("the quotient of a/b\n , a/b");
+    return 0;
+    
+
+}
